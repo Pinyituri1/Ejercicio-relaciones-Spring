6904a7ce008b494983b2d504f11849fcce38b3ee
Pinyituri1 <jfernandotrujillo@ucundinamarca.edu.co>
2025-04-21 20:00:00 -0500

    first commit

--- src/main/java/com/example/BDPostgres/Repository/ClienteRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.BDPostgres.Repository;
-
-import com.example.BDPostgres.Model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
-}
